337d0a887010012f0d1b855973280628b1eb414f
Erik Brouwer <erik.brouwer@ict.nl>
2016-09-22 15:25:33 +0200

    Changed libsodium support, all versions starting from 1.0.8 are allowed.

--- usef-build/usef-core/usef-core-transport/src/main/java/energy/usef/core/util/encryption/NaCl.java
+++ usef-build/usef-core/usef-core-transport/src/main/java/energy/usef/core/util/encryption/NaCl.java
@@ -16,6 +16,7 @@
 
 package energy.usef.core.util.encryption;
 
+import energy.usef.core.util.VersionUtil;
 import jnr.ffi.LibraryLoader;
 import jnr.ffi.annotations.In;
 import jnr.ffi.annotations.Out;
@@ -27,7 +28,7 @@ import jnr.ffi.types.u_int64_t;
  */
 public class NaCl {
     private static final String LIBRARY_NAME = "sodium";
-    private static final String VERSION = "1.0.10";
+    private static final String VERSION = "1.0.8";
 
     /**
      * Creates a sodium instance.
@@ -37,9 +38,9 @@ public class NaCl {
     public static Sodium sodium() {
         Sodium sodium = SingletonHolder.SODIUM_INSTANCE;
 
-        if (!(sodium.sodium_version_string().compareTo(VERSION) == 0)) {
-            String message = String.format("Unsupported libsodium version: %s. Please USE version %s",
-                    sodium.sodium_version_string(), VERSION);
+        if (VersionUtil.compareVersions(VERSION, sodium.sodium_version_string()) > 0) {
+            String message = String.format("Unsupported libsodium version: %s. Please update",
+                    sodium.sodium_version_string());
             throw new UnsupportedOperationException(message);
         }
         return sodium;
--- usef-environment-tool/src/main/java/energy/usef/core/util/encryption/NaCl.java
+++ usef-environment-tool/src/main/java/energy/usef/core/util/encryption/NaCl.java
@@ -16,6 +16,7 @@
 
 package energy.usef.core.util.encryption;
 
+import energy.usef.environment.tool.util.VersionUtil;
 import jnr.ffi.LibraryLoader;
 import jnr.ffi.annotations.In;
 import jnr.ffi.annotations.Out;
@@ -27,7 +28,7 @@ import jnr.ffi.types.u_int64_t;
  */
 public class NaCl {
     private static final String LIBRARY_NAME = "sodium";
-    private static final String VERSION = "1.0.10";
+    private static final String VERSION = "1.0.8";
     /**
      * Creates a sodium instance.
      *
@@ -36,8 +37,8 @@ public class NaCl {
     public static Sodium sodium() {
         Sodium sodium = SingletonHolder.SODIUM_INSTANCE;
 
-        if (!(sodium.sodium_version_string().compareTo(VERSION) == 0)) {
-            String message = String.format("Unsupported libsodium version: %s. Please USE version %s",
+        if (VersionUtil.compareVersions(VERSION, sodium.sodium_version_string()) > 0) {
+            String message = String.format("Unsupported libsodium version: %s.",
                     sodium.sodium_version_string(), VERSION);
             throw new UnsupportedOperationException(message);
         }
