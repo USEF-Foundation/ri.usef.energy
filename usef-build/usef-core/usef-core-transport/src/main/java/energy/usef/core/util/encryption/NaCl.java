f108dd7d539c70c4023292603d1ec7dc447adb98
drighart <david.righart@alliander.com>
2016-12-17 23:15:14 +0100

    Changed back

--- usef-build/usef-core/usef-core-transport/src/main/java/energy/usef/core/util/encryption/NaCl.java
+++ usef-build/usef-core/usef-core-transport/src/main/java/energy/usef/core/util/encryption/NaCl.java
@@ -27,7 +27,7 @@ import jnr.ffi.types.u_int64_t;
  * NaCl class to load sodium.
  */
 public class NaCl {
-    private static final String LIBRARY_NAME = "libsodium";
+    private static final String LIBRARY_NAME = "sodium";
     private static final String VERSION = "1.0.11";
 
     /**
