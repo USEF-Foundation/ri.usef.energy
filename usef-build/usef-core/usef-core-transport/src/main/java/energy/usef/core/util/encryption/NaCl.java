2bb623386ced4eb6c9cbef6ebc98435db47d6d1a
drighart <david.righart@alliander.com>
2016-12-17 23:11:20 +0100

    Make libsodium work

--- usef-build/usef-core/usef-core-transport/src/main/java/energy/usef/core/util/encryption/NaCl.java
+++ usef-build/usef-core/usef-core-transport/src/main/java/energy/usef/core/util/encryption/NaCl.java
@@ -27,8 +27,8 @@ import jnr.ffi.types.u_int64_t;
  * NaCl class to load sodium.
  */
 public class NaCl {
-    private static final String LIBRARY_NAME = "sodium";
-    private static final String VERSION = "1.0.8";
+    private static final String LIBRARY_NAME = "libsodium";
+    private static final String VERSION = "1.0.11";
 
     /**
      * Creates a sodium instance.
